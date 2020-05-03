08a1ca1cc2ae8d3db4666c92193619f5e5fd82b6
gopirajus <gopirajus@localhost>
2020-05-04 00:17:24 +0100

    i am copying central part

---
+++ ARRAY/src/DaimondShapePatern.java
@@ -0,0 +1,20 @@
+
+public class DaimondShapePatern {
+
+	public static void main(String[] args)
+	
+	{
+		// TODO Auto-generated method stub
+		
+		for(int i =1;i<=4;i++)
+		{
+			for(int j =i;j>=0;j--)
+			{
+				System.out.print("9");
+			}
+		}
+		
+
+	}
+
+}
